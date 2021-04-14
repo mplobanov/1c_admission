66ff214f68c81d6bac7629fb7bed90afd280b512
Mikhail Lobanov <mp.lobanov@yandex.ru>
2021-04-14 21:44:06 +0300

    not all filenames supported now

--- binary_difference/utils/filenames.py
+++ binary_difference/utils/filenames.py
@@ -2,6 +2,8 @@ from .list_logic import list_and
 
 
 def make_diff_name(old: str, new: str) -> str:
+    assert '>' not in old, 'filenames with > are not supperted yet'
+    assert '>' not in new, 'filenames with > are not supperted yet'
     return "{}->{}.diff".format(old, new)
 
 
@@ -11,3 +13,11 @@ def check_diff_name(old: str, diff: str) -> bool:
         diff.startswith("{}->".format(old)),
     ]
     return list_and(conditions)
+
+
+def get_new_filename(old: str, diff: str) -> str:
+    return diff[diff.rindex('>') + 1: diff.rindex('.diff')]
+
+
+def make_recovery_name(new: str) -> str:
+    return '{}.recovered'.format(new)
