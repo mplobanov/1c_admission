d66a4aeea7c569325543735af8b9c74da94af1f7
Mikhail Lobanov <mp.lobanov@yandex.ru>
2021-04-14 21:03:26 +0300

    unnecessary files deleted

--- examples/basics.py
+++ examples/basics.py
@@ -1,12 +1,13 @@
 from binary_difference.calculate import calculate_difference
 from binary_difference.recover import recover
-from tools.random import randbytes
 
 
 old_file = open('files/file1', 'rb')
 new_file = open('files/file2', 'rb')
 diff_file = open('files/file1_diff_file2', 'wb')
+
 calculate_difference(old_file, new_file, diff_file)
+
 old_file.close()
 new_file.close()
 diff_file.close()
@@ -14,8 +15,9 @@ diff_file.close()
 old_file = open('files/file1', 'rb')
 diff_file = open('files/file1_diff_file2', 'rb')
 recovered_file = open('files/recovered_file2', 'wb')
+
 recover(old_file, diff_file, recovered_file)
+
 old_file.close()
 diff_file.close()
 recovered_file.close()
-print(randbytes(10))
